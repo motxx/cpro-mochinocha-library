eee70cf8eade6c6d74d33fed5265d4a29d5755e5
motxx <motohiko.ave@gmail.com>
2016-06-30 04:47:44 +0900

    tree_isomorphism.cpp

---
+++ Tree/tree_isomorphism.cpp
@@ -0,0 +1,25 @@
+namespace tree {
+
+using tree_type = vector<vector<int>>;
+
+std::string to_string(tree_type const& t, int v, int par = -1) {
+  ostringstream oss;
+  oss << '(';
+  vector<string> children;
+  children.resize(t[v].size());
+  for(int u: t[v]) {
+    if(u == par) continue;
+    children.emplace_back(to_string(t, u, v));
+  }
+  sort(children.begin(), children.end());
+  for(auto const& child: children)
+    oss << child;
+  oss << ')';
+  return oss.str();
+}
+
+bool tree_isomorphism(tree_type const& a, tree_type const& b) {
+  return to_string(a, 0) == to_string(b, 0);
+}
+
+}
